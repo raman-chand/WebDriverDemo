5c737699fc9083ed9b2085b576474461478048ac
Ramanpreet Chand <rchand595@gmail.com>
2017-10-01 13:13:14 -0400

    commit Firefox and Microsoft drivers

--- WebDriverDemo/Program.cs
+++ WebDriverDemo/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Edge;
 
 namespace WebDriverDemo
 {
@@ -13,9 +14,17 @@ namespace WebDriverDemo
     {
         static void Main(string[] args)
         {
+            // GeckoDriver added to PATH var
             using(IWebDriver driver = new FirefoxDriver())
             {
                 driver.Navigate().GoToUrl("http://wwww.google.com");
+            }
+
+            // MicrosoftDriver located in libraryPath
+            string libraryPath = "C:\\Libraries\\";
+            using (IWebDriver driverEdge = new EdgeDriver(@libraryPath))
+            {
+                driverEdge.Navigate().GoToUrl("http://youtube.com");
             }
         }
     }
