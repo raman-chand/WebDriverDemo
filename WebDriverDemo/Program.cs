5671513042dcd3894748e290d2e5958e111163b7
Ramanpreet Chand <rchand595@gmail.com>
2017-10-01 15:05:14 -0400

    commit work in WebDriver basics

--- WebDriverDemo/Program.cs
+++ WebDriverDemo/Program.cs
@@ -18,7 +18,19 @@ namespace WebDriverDemo
             // GeckoDriver added to PATH var
             using(IWebDriver driver = new FirefoxDriver())
             {
-                driver.Navigate().GoToUrl("http://wwww.google.com");
+                driver.Navigate().GoToUrl("http://www.google.com");
+
+                // find an element tied to google's search box
+                IWebElement searchBox = driver.FindElement(By.Name("q"));
+                // send a key string to the search box.
+                searchBox.SendKeys("pluralsight");
+
+                searchBox.Submit();
+                
+                // click the Images link to view pluralsight images
+                IWebElement elem = driver.FindElements(By.ClassName("hdtb-mitem"))[4];
+                IWebElement imagesLink = elem.FindElement(By.TagName("a"));
+                imagesLink.Click();
             }
 
             // MicrosoftDriver located in libraryPath
@@ -28,6 +40,7 @@ namespace WebDriverDemo
                 driverEdge.Navigate().GoToUrl("http://youtube.com");
             }
 
+            // ChromeDriver located in libraryPath
             using (IWebDriver driverChrome = new ChromeDriver(@libraryPath))
             {
                 driverChrome.Navigate().GoToUrl("http://www.cnn.com");
