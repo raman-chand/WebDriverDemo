170ab614a163ea6a6ad2fed34557b510be42f910
Ramanpreet Chand <rchand595@gmail.com>
2017-10-01 12:52:49 -0400

    initial commit of selenium webdriver

--- WebDriverDemo/Program.cs
+++ WebDriverDemo/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
 namespace WebDriverDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using(IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Navigate().GoToUrl("http://wwww.google.com");
+            }
         }
     }
 }
